0c74627cc33effbac913b8a92a9df6146ec86249
k-nasa <htilcs1115@gmail.com>
2018-09-08 21:08:57 +0900

    add/ ヘルパー関数を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
-fn main() {
-    println!("Hello, world!");
+extern crate num;
+extern crate image;
+
+use num::Complex;
+use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::io::Result;
+
+fn escape_time(c :Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i)
+        }
+    }
+    None
+}
+
+// コマンドライン引数のパーサー
+fn parse_paier<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            // 二重マッチう〜んという感じ
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+// カンマ区切りのペアをパースして複素数を返す
+fn parse_to_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_paier(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  uper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (
+        lower_right.re - uper_left.re,
+        uper_left.im - lower_right.im
+    );
+
+    Complex {
+        re: uper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: uper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          uper_left: Complex<f64>,
+          lower_right: Complex<f64>
+)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.1 {
+            let point = pixel_to_point(bounds,
+                                       (column, row),
+                                       uper_left, lower_right
+            );
+
+            pixels[row *bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                };
+        }
+    }
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<()>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
